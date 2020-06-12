f0a8efc9b34ccf75f855ecbbdd52bcec3f0a6c1d
Johnjalani <jfrbph12@hotmail.com>
2020-06-12 13:39:34 +0800

    update the landing page and UI

--- NationalMerchantCenter/Controllers/DataBreachController.cs
+++ NationalMerchantCenter/Controllers/DataBreachController.cs
@@ -12,6 +12,7 @@ namespace Codebase.Website.Nmc.Controllers
         // GET: DataBreach
         public ActionResult Index()
         {
+            ViewBag.Video = ConfigurationManager.AppSettings["DataBreachVideo"];
             return View();
         }
         public ActionResult Video()
